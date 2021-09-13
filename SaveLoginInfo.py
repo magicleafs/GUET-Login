7cf960a91490aeaa89abd5461b0c8d844df80085
magicleafs <89396764+magicleafs@users.noreply.github.com>
2021-09-13 21:19:57 +0800

    Add files via upload

--- SaveLoginInfo.py
+++ SaveLoginInfo.py
@@ -2,26 +2,64 @@ import tkinter as tk
 from tkinter import messagebox
 import time
 import sys
+import os.path
 
 
-def SaveInfo(m_sid, m_pwd):
+def SaveInfo(m_sid, m_pwd, m_var1, m_var2, m_var3, m_var4):
     fp = open("D:/Weblogin.txt", "w")
     sid = m_sid
     pwd = m_pwd
+    var1 = m_var1
+    var2 = m_var2
+    var3 = m_var3
+    var4 = m_var4
     print(sid, file=fp)
     print(pwd, file=fp)
+    print(var1, file=fp)
+    print(var2, file=fp)
+    print(var3, file=fp)
+    print(var4, file=fp)
     fp.close()
 
 
 def ReadInfo():
     fp = open("D:/Weblogin.txt", "r")
-    read_ = fp.read()
-    sid = read_[0:read_.find('\n')]
-    pwd = read_[read_.find('\n') + 1:len(read_) - 1]
+    sid = fp.readline()
+    sid.strip('\n')
+    sid = sid.strip('\n')
+    pwd = fp.readline()
+    pwd = pwd.strip('\n')
+    var1 = fp.readline()
+    var1 = var1.strip('\n')
+    var2 = fp.readline()
+    var2 = var2.strip('\n')
+    var3 = fp.readline()
+    var3 = var3.strip('\n')
+    var4 = fp.readline()
+    var4 = var4.strip('\n')
+    read_result = [sid, pwd, var1, var2, var3, var4]
+    fp.close()
+    return read_result
 
 
 def find_isp(var3):
     flag = var3.get()
+    if flag == 2:
+        print('登录成功')
+        tk.messagebox.showinfo(title='提示', message='登录成功\n当前运营商：中国移动')
+    elif flag == 3:
+        print('登录成功')
+        tk.messagebox.showinfo(title='提示', message='登录成功\n当前运营商：中国电信')
+    elif flag == 4:
+        print('登录成功')
+        tk.messagebox.showinfo(title='提示', message='登录成功\n当前运营商：中国联通')
+    else:
+        print('登录成功')
+        tk.messagebox.showinfo(title='提示', message='登录成功\n当前运营商：校园网')
+
+
+def find_isp2(var3):
+    flag = var3
     if flag == 2:
         print('登录成功')
         tk.messagebox.showinfo(title='提示', message='登录成功\n当前运营商：中国移动')
@@ -42,3 +80,23 @@ def AutoExit():
         sys.exit()
     else:
         print(time.time())
+
+
+def Read_Var4():
+    return int(ReadInfo()[5])
+
+
+def Read_Var3():
+    return int(ReadInfo()[4])
+
+
+def Read_Var2():
+    return int(ReadInfo()[3])
+
+
+def isExist():
+    return os.path.isfile('D:\Weblogin.txt')
+
+
+def reset():
+    os.system('del D:\Weblogin.txt')
--- main.py
+++ main.py
@@ -6,14 +6,44 @@ import SaveLoginInfo
 from tkinter import messagebox
 
 
+def AutoLogin():
+
+    global sid, pwd
+    m_tuple = SaveLoginInfo.ReadInfo()
+    sid = m_tuple[0]
+    pwd = m_tuple[1]
+    m_var3 = int(m_tuple[4])
+    # 判断运营商
+    if m_var3 == 2:
+        url = "http://10.0.1.5/drcom/login?callback=dr1003&DDDDD=" + sid + "%40cmcc" + "&upass=" + pwd + "&0MKKey=123456&R1=0&R2=&R3=0&R6=0&para=00&v6ip=&terminal_type=1&lang=zh-cn&jsVersion=4.1&v=1058&lang=zh "
+    elif m_var3 == 3:
+        url = "http://10.0.1.5/drcom/login?callback=dr1003&DDDDD=" + sid + "%40telecom" + "&upass=" + pwd + "&0MKKey=123456&R1=0&R2=&R3=0&R6=0&para=00&v6ip=&terminal_type=1&lang=zh-cn&jsVersion=4.1&v=1058&lang=zh "
+    elif m_var3 == 4:
+        url = "http://10.0.1.5/drcom/login?callback=dr1003&DDDDD=" + sid + "%40unicom" + "&upass=" + pwd + "&0MKKey=123456&R1=0&R2=&R3=0&R6=0&para=00&v6ip=&terminal_type=1&lang=zh-cn&jsVersion=4.1&v=1058&lang=zh "
+    else:
+        url = "http://10.0.1.5/drcom/login?callback=dr1003&DDDDD=" + sid + "&upass=" + pwd + "&0MKKey=123456&R1=0&R2=&R3=0&R6=0&para=00&v6ip=&terminal_type=1&lang=zh-cn&jsVersion=4.1&v=1058&lang=zh "
+
+    req = requests.get(url)
+    req = str(req.text)
+    find_str = 'result":1'
+    if find_str in req:
+        if var4.get() == 1:
+            sys.exit()
+        else:
+            SaveLoginInfo.find_isp2(m_var3)
+    else:
+        print('登录失败')
+        tk.messagebox.showinfo(title='提示', message='登录失败\n账号或密码错误')
+
+
 # 登陆实现函数
 def login():
     global sid, pwd, var1
     sid = u.get()
     pwd = p.get()
-    #判断运营商
+    # 判断运营商
     if var3.get() == 2:
-        url = "http://10.0.1.5/drcom/login?callback=dr1003&DDDDD=" + sid + "%40cmcc" + "&upass=" + pwd + "&0MKKey=123456&R1=0&R2=&R3=0&R6=0&para=00&v6ip=&terminal_type=1&lang=zh-cn&jsVersion=4.1&v=1058&lang=zh "                                                                                                 "&v=1058&lang=zh "
+        url = "http://10.0.1.5/drcom/login?callback=dr1003&DDDDD=" + sid + "%40cmcc" + "&upass=" + pwd + "&0MKKey=123456&R1=0&R2=&R3=0&R6=0&para=00&v6ip=&terminal_type=1&lang=zh-cn&jsVersion=4.1&v=1058&lang=zh "
     elif var3.get() == 3:
         url = "http://10.0.1.5/drcom/login?callback=dr1003&DDDDD=" + sid + "%40telecom" + "&upass=" + pwd + "&0MKKey=123456&R1=0&R2=&R3=0&R6=0&para=00&v6ip=&terminal_type=1&lang=zh-cn&jsVersion=4.1&v=1058&lang=zh "
     elif var3.get() == 4:
@@ -28,7 +58,7 @@ def login():
         if var1.get() == 0:
             print('已保存')
             tk.messagebox.showinfo(title='提示', message='已保存')
-            SaveLoginInfo.SaveInfo(sid, pwd)
+            SaveLoginInfo.SaveInfo(sid, pwd, var1.get(), var2.get(), var3.get(),var4.get())
     else:
         print('登录失败')
         tk.messagebox.showinfo(title='提示', message='登录失败\n账号或密码错误')
@@ -52,6 +82,7 @@ p = tk.StringVar()
 password = tk.Entry(window, show="*", textvariable=p)
 password.pack()
 
+
 var1 = tk.IntVar()
 Save = tk.Checkbutton(window, text='保存密码', variable=var1, onvalue=0, offvalue=1)  # 保存密码复选框
 Save.pack()
@@ -59,6 +90,13 @@ Save.pack()
 var2 = tk.IntVar()
 Autologin = tk.Checkbutton(window, text='自动登录', variable=var2, onvalue=1, offvalue=0)
 Autologin.pack()
+
+var4 = tk.IntVar()
+AutoExit= tk.Checkbutton(window, text='登录成功后退出', variable=var4, onvalue=1, offvalue=0)
+AutoExit.pack()
+
+
+
 """
 t = tk.Text(window,height=2)
 t.pack()
@@ -67,6 +105,13 @@ l3 = tk.Label(text="选择运营商（默认校园网）:")
 l3.pack()
 
 var3 = tk.IntVar()
+if SaveLoginInfo.isExist():
+    var4.set(SaveLoginInfo.Read_Var4())
+    var2.set(SaveLoginInfo.Read_Var2())
+    var3.set(SaveLoginInfo.Read_Var3())
+    if var2.get()==1:
+        AutoLogin()
+
 
 isp1 = tk.Radiobutton(window, text='校园网', variable=var3, value=1)
 isp1.pack(side='left')
@@ -82,5 +127,7 @@ isp4.pack(side='right')
 
 b = tk.Button(window, text="登入网络", bg="green", command=login)  # 登录按钮
 b.pack()
+b2 = tk.Button(window, text="一键重置", bg="red", command=SaveLoginInfo.reset)  # 登录按钮
+b2.pack()
 
 window.mainloop()
