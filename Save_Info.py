9e32b91bc33875021018d82e60e5b8d6f00af6f2
magicleafs <89396764+magicleafs@users.noreply.github.com>
2021-11-30 15:39:30 +0100

    Add files via upload

--- Save_Info.py
+++ Save_Info.py
@@ -1,11 +1,12 @@
-from tkinter import messagebox
+from tkinter import messagebox, filedialog
 import time
 import sys
-import os.path
+import os
+import base64
 
 
 # 保存相关信息
-def SaveInfo(m_sid, m_pwd, m_var1, m_var2, m_var3, m_var4):
+def SaveInfo(m_sid, m_pwd, m_var1, m_var2, m_var3, m_var4, m_var5):  # , file_path):
     fp = open(r"D:\Weblogin.txt", "w")
     sid = m_sid
     pwd = m_pwd
@@ -13,12 +14,23 @@ def SaveInfo(m_sid, m_pwd, m_var1, m_var2, m_var3, m_var4):
     var2 = m_var2
     var3 = m_var3
     var4 = m_var4
+    var5 = m_var5
+    # path = file_path
     print(sid, file=fp)
     print(pwd, file=fp)
     print(var1, file=fp)
     print(var2, file=fp)
     print(var3, file=fp)
     print(var4, file=fp)
+    print(var5, file=fp)
+    # print(path, file=fp)
+    fp.close()
+
+
+def save_path():
+    path_ = filedialog.askopenfilename()
+    fp = open(r"D:\Weblogin.txt", "a+")
+    print(path_, file=fp)
     fp.close()
 
 
@@ -37,7 +49,9 @@ def ReadInfo():
     var3 = var3.strip('\n')
     var4 = fp.readline()
     var4 = var4.strip('\n')
-    read_result = [sid, pwd, var1, var2, var3, var4]
+    var5 = fp.readline()
+    var5 = var5.strip('\n')
+    read_result = [sid, pwd, var1, var2, var3, var4, var5]
     fp.close()
     return read_result
 
@@ -68,6 +82,10 @@ def AutoExit():
         print(time.time())
 
 
+def Read_Var5():
+    return int(ReadInfo()[6])
+
+
 def Read_Var4():
     return int(ReadInfo()[5])
 
@@ -78,6 +96,10 @@ def Read_Var3():
 
 def Read_Var2():
     return int(ReadInfo()[3])
+
+
+# def Read_File_path():
+# return ReadInfo()[7]
 
 
 def isExist():
@@ -98,8 +120,37 @@ def reset():
     os.system(r'del D:\Weblogin.txt')
 
 
+
+
+
 def Read_Var1():
     if isExist():
         if isEmpty() == 0:
             return int(ReadInfo()[2])
 
+
+def Read_path():
+    with open(r"D:\Weblogin.txt", 'r') as x:
+        lines = x.readlines()
+    try:
+        lines[7] = lines[7].strip('\n')
+        x.close()
+        return lines[7]
+    except:
+        return 0
+
+
+def start_run(path_):
+    shell = 'shell:start '+ path_
+    print(shell)
+    return shell
+"""
+对图片进行转码，否则打包后的文件无法使用
+open_icon = open("denglu.ico", "rb")
+b64str = base64.b64encode(open_icon.read())
+open_icon.close()
+write_data = "img = %s" % b64str
+f = open("icon.py", "w+")
+f.write(write_data)
+f.close()
+"""
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import base64
+import os
 import requests
 import tkinter as tk
 import Save_Info
 from tkinter import messagebox
+import win32api
+import win32con
+from tkinter import filedialog
+from icon import img
 
 
 # 自动登录
@@ -11,6 +17,35 @@ def AutoLogin():
     p.set(m_tuple[1])
     var3.set(int(m_tuple[4]))
     login()
+
+
+def AutoRun():
+    if var5.get() == 1 and file_path != "" and ".exe" in file_path:
+        name = 'GUET_Login'  # 要添加的项值名称
+        path = file_path  # 要添加的exe路径
+        # 注册表项名
+        KeyName1 = 'Software\\Microsoft\\Windows\\CurrentVersion\\Run'
+        # 异常处理
+        try:
+            key = win32api.RegOpenKey(win32con.HKEY_CURRENT_USER, KeyName1, 0, win32con.KEY_ALL_ACCESS)
+            win32api.RegSetValueEx(key, name, 0, win32con.REG_SZ, path)
+            win32api.RegCloseKey(key)
+            print('添加成功！')
+        except:
+            print('添加失败')
+
+    else:
+        name = 'GUET_Login'  # 要添加的项值名称
+        # 注册表项名
+        KeyName1 = 'Software\\Microsoft\\Windows\\CurrentVersion\\Run'
+        # 异常处理
+        try:
+            key = win32api.RegOpenKey(win32con.HKEY_CURRENT_USER, KeyName1, 0, win32con.KEY_ALL_ACCESS)
+            win32api.RegDeleteValue(key, name)
+            win32api.RegCloseKey(key)
+            print('已移除')
+        except:
+            print('移除失败')
 
 
 # 登陆实现函数
@@ -36,69 +71,95 @@ def login():
         if var1.get() == 1:
             print('已保存')
             tk.messagebox.showinfo(title='提示', message='已保存')
-            Save_Info.SaveInfo(sid, pwd, var1.get(), var2.get(), var3.get(), var4.get())
+            Save_Info.SaveInfo(sid, pwd, var1.get(), var2.get(), var3.get(), var4.get(), var5.get())
         else:
-            Save_Info.SaveInfo(sid, pwd, var1.get(), var2.get(), var3.get(), var4.get())
+            Save_Info.SaveInfo(sid, pwd, var1.get(), var2.get(), var3.get(), var4.get(), var5.get())
     else:
         print('登录失败')
-        tk.messagebox.showinfo(title='提示', message='登录失败\n账号或密码错误')
+        tk.messagebox.showinfo(title='提示', message='登录失败\n账号或密码错误\n')
 
 
+# --------------------------------------------------初始化
+
+fp = open(r"D:\Weblogin.txt", "a+")
+fp.close()
+# tmp = open("tmp.ico", "wb+")
+# tmp.write(base64.b64decode(img))
+# tmp.close()
 window = tk.Tk()
 window.title("桂电网络登录认证")
 window.geometry("400x300")  # 窗口大小
+window.iconbitmap()
+"""
+l_1 = tk.Label(window, text="选中本程序路径，以此完成启动需要\n")
+l_1.place(x=20, y=10)
 
-sid = '?'
-l1 = tk.Label(text="账号:")
-l1.pack()
+if Save_Info.isEmpty() == 0:
+    if Save_Info.Read_path() == 0:
+        Save_Info.save_path()
+        file_path = Save_Info.Read_path()
+    else:
+        file_path = Save_Info.Read_path()
+else:
+    file_path = filedialog.askopenfilename()
+
+l_1.destroy()
+"""
+# ---------------------------------------------------
+l1 = tk.Label(window, text="账号:")
+l1.place(x=100, y=10)
+
 u = tk.StringVar()
 number = tk.Entry(window, textvariable=u)
-number.pack()
+number.place(x=140, y=10)
 
-pwd = '?'
-l2 = tk.Label(text="密码:")
-l2.pack()
+l2 = tk.Label(window, text="密码:")
+l2.place(x=100, y=40)
 p = tk.StringVar()
 password = tk.Entry(window, show="*", textvariable=p)
-password.pack()
+password.place(x=140, y=40)
 
 var1 = tk.IntVar()
 Save = tk.Checkbutton(window, text='保存密码', variable=var1, onvalue=1, offvalue=0)  # 保存密码复选框
-Save.pack()
+Save.place(x=10, y=70)
 
 var2 = tk.IntVar()
 Autologin = tk.Checkbutton(window, text='自动登录', variable=var2, onvalue=1, offvalue=0)
-Autologin.pack()
+Autologin.place(x=10, y=100)
 
 var4 = tk.IntVar()
 AutoExit = tk.Checkbutton(window, text='登录成功后退出', variable=var4, onvalue=1, offvalue=0)
-AutoExit.pack()
+AutoExit.place(x=10, y=130)
+
+var5 = tk.IntVar()
+AutoStart = tk.Checkbutton(window, text='开机自启', variable=var5, onvalue=1, offvalue=0)#, command=AutoRun)
+AutoStart.place(x=10, y=160)
 
 """
 t = tk.Text(window,height=2)
 t.pack()
 """
 l3 = tk.Label(text="选择运营商（默认校园网）:")
-l3.pack()
+l3.place(x=240, y=70)
 
 var3 = tk.IntVar()
 
 isp1 = tk.Radiobutton(window, text='校园网', variable=var3, value=1)
-isp1.pack(side='left')
+isp1.place(x=300, y=100)
 
 isp2 = tk.Radiobutton(window, text='中国移动', variable=var3, value=2)
-isp2.pack(side='right')
+isp2.place(x=300, y=130)
 
 isp3 = tk.Radiobutton(window, text='中国电信', variable=var3, value=3)
-isp3.pack(side='left')
+isp3.place(x=300, y=160)
 
 isp4 = tk.Radiobutton(window, text='中国联通', variable=var3, value=4)
-isp4.pack(side='right')
+isp4.place(x=300, y=190)
 
 b = tk.Button(window, text="登入网络", bg="green", command=login)  # 登录按钮
-b.pack()
+b.place(x=170, y=150)
 b2 = tk.Button(window, text="一键重置", bg="red", command=Save_Info.reset)  # 清除按钮
-b2.pack()
+b2.place(x=170, y=270)
 
 if Save_Info.isExist():
     if Save_Info.isEmpty() == 0:
@@ -106,6 +167,7 @@ if Save_Info.isExist():
         var4.set(Save_Info.Read_Var4())
         var2.set(Save_Info.Read_Var2())
         var3.set(Save_Info.Read_Var3())
+        var5.set(Save_Info.Read_Var5())
     if var1.get() == 1:
         m_tuple2 = Save_Info.ReadInfo()
         u.set(m_tuple2[0])
@@ -114,5 +176,5 @@ if Save_Info.isExist():
         if var1.get() == 1:
             AutoLogin()
 
-
+# os.remove("tmp.ico")
 window.mainloop()
